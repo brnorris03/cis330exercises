6e9d772fea7599ebef06cfa39f0688eebd208432
Boyana Norris <brnorris03@gmail.com>
2014-02-11 16:22:25 -0800

    lecture 16

---
+++ lecture16/farmer.cpp
@@ -0,0 +1,25 @@
+/*
+ * farmer.cpp
+ *
+ *  Created on: Feb 3, 2014
+ *      Author: norris
+ */
+
+#include <iostream>
+#include "sheep.hpp"
+#include "farmer.hpp"
+
+Farmer::Farmer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Farmer::~Farmer() {
+	// TODO Auto-generated destructor stub
+}
+
+void
+Farmer::shearSheep(Sheep &s) {
+	s.wool = Wool("Sheared");
+	std::cout << "Farmer shearing sheep ..."<<  std::endl;
+}
---
+++ lecture16/sheep.cpp
@@ -0,0 +1,112 @@
+/*
+ * sheep.cpp
+ *
+ *  Created on: Feb 2, 2014
+ *      Author: norris
+ */
+
+#include <string>
+#include <iostream>
+#include "sheep.hpp"
+
+using namespace std;
+
+/*
+ * Wool class implementation
+ */
+
+Wool::Wool() : color("White") {
+}
+
+Wool::Wool(string c) : color(c) {
+}
+
+// Copy constructor:
+Wool::Wool(const Wool& other) : color(other.color) {
+	cout << "Wool copy constructor" << endl;
+}
+
+// Move constructor
+Wool::Wool(Wool&& other) : color(other.color) {
+	cout << "Wool move constructor" << endl;
+}
+
+Wool& 
+Wool::operator=(const Wool& rhs)  {
+	this->color = rhs.color;
+}
+
+Wool& 
+Wool::operator=(Wool&& rhs) {
+	this->color = rhs.color;
+}
+		        
+
+
+
+void
+Wool::setColor(string color) {
+	this->color = color;
+}
+
+const string
+Wool::getColor() {
+	return this->color;
+}
+
+
+/*
+ * Sheep class implementation
+ */
+
+Sheep::Sheep() : Quadruped() {
+}
+
+// Copy constructor
+Sheep::Sheep(const Sheep& rhs) : wool(rhs.wool) {
+}
+
+// Move constructor
+Sheep::Sheep(Sheep&& rhs) : wool(rhs.wool) {
+}
+
+// Destructor
+Sheep::~Sheep() {
+}
+
+void
+Sheep::eat() {
+	cout << wool.getColor() << " sheep grazing some grass...baa!" << endl;
+}
+
+void
+Sheep::drink() {
+	cout << wool.getColor() << " sheep drinking from trough...baa!" << endl;
+}
+
+void
+Sheep::sleep() {
+	cout << wool.getColor() << " sheep sleeping in barn...zzz!" << endl;
+}
+
+void
+Sheep::growWool(const Wool &w) {
+	this->wool = w;
+	cout << wool.getColor() << " sheep growing some wool..." << endl;
+}
+
+void
+Sheep::growWool(Wool &&w) {		// rvalue reference
+	this->wool = w;
+	cout << "Sheep growing some " << wool.getColor() << " wool more efficiently." << endl;
+}
+
+const Wool&
+Sheep::getWool() {
+	return this->wool;
+}
+
+string
+Sheep::toString() {
+	return this->wool.getColor() + " sheep";
+}
